a2a7638d4f92214b7208407b10d4a07cfb740f07
AndreaCZ16 <135395365+AndreaCZ16@users.noreply.github.com>
2023-09-21 18:09:35 -0500

    Update Admin.java

--- src/main/java/pe/pamperurpet/pamperurpetapp/entities/Admin.java
+++ src/main/java/pe/pamperurpet/pamperurpetapp/entities/Admin.java
@@ -1,4 +1,13 @@
 package pe.pamperurpet.pamperurpetapp.entities;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Admin {
+
 }
