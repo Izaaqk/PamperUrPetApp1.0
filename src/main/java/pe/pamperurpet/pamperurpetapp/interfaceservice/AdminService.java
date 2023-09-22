e717b9615bf2cb80a84b1268026d6f7770bd8bdb
AndreaCZ16 <135395365+AndreaCZ16@users.noreply.github.com>
2023-09-21 21:42:55 -0500

    Update AdminService.java

--- src/main/java/pe/pamperurpet/pamperurpetapp/interfaceservice/AdminService.java
+++ src/main/java/pe/pamperurpet/pamperurpetapp/interfaceservice/AdminService.java
@@ -1,4 +1,24 @@
 package pe.pamperurpet.pamperurpetapp.interfaceservice;
 
+import pe.pamperurpet.pamperurpetapp.entities.Mascota;
+import pe.pamperurpet.pamperurpetapp.entities.Paseador;
+import pe.pamperurpet.pamperurpetapp.entities.Propietario;
+
+import java.util.List;
+
 public interface AdminService {
+    public Propietario register(Propietario propietario);
+    public List<Propietario> listPropietarios();
+    public Propietario updatePropietario(Propietario propietario) throws Exception;
+    public Propietario deletePropietario(Long codigo) throws Exception;
+
+    public Paseador register(Paseador paseador);
+    public List<Paseador> listPaseador();
+    public Paseador updatePaseador(Paseador paseador) throws Exception;
+    public Paseador deletePaseador (Long codigo) throws Exception;
+
+    public Mascota register(Mascota mascota);
+    public List<Mascota> listMascota();
+    public Mascota updateMascota(Mascota mascota) throws Exception;
+    public Mascota deleteMascota(Long codigo) throws Exception;
 }
